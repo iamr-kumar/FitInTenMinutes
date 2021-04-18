bb57d60112f6ed162d96fc874ecb43da07b4241e
Ritik <ritik.kumar006@gmail.com>
2021-04-19 02:43:01 +0530

    bmi unit metrics done

--- app/src/main/java/com/example/fitinsevenminutes/BmiActitvity.kt
+++ app/src/main/java/com/example/fitinsevenminutes/BmiActitvity.kt
@@ -2,7 +2,11 @@ package com.example.fitinsevenminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.fitinsevenminutes.databinding.ActivityBmiActitvityBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BmiActitvity : AppCompatActivity() {
     private lateinit var binding: ActivityBmiActitvityBinding
@@ -15,9 +19,84 @@ class BmiActitvity : AppCompatActivity() {
         val actionBar = supportActionBar
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.title = "CALCULATE BMI"
+
         }
         binding.toolbarBmiActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding.btnCalculateUnits.setOnClickListener {
+            if(!validateMetricUnits()) {
+                Toast.makeText(this, "Please enter valid information", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val heightValue: Float = binding.etMetricUnitHeight.text.toString().toFloat() / 100
+                val weightValue: Float = binding.etMetricUnitWeight.text.toString().toFloat()
+                val bmi: Float = weightValue / (heightValue * heightValue)
+
+                displayBMIResult(bmi)
+            }
+        }
+
+    }
+
+    private fun validateMetricUnits(): Boolean {
+        var isValid = true
+
+        if(binding.etMetricUnitHeight.text.toString().isEmpty()) {
+            isValid = false
+        }
+        else if(binding.etMetricUnitWeight.text.toString().isEmpty()) {
+            isValid = false
+        }
+        return isValid
+    }
+
+    private fun displayBMIResult(bmi: Float) {
+        val bmiLabel: String
+        val bmiDescription: String
+
+        if (bmi.compareTo(15f) <= 0) {
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
+        ) {
+            bmiLabel = "Severely underweight"
+            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
+        ) {
+            bmiLabel = "Underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
+        ) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
+        ) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0
+        ) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        binding.tvBmi.visibility = View.VISIBLE
+        binding.tvBmiValue.visibility = View.VISIBLE
+        binding.tvBmiType.visibility = View.VISIBLE
+        binding.tvBmiDesc.visibility = View.VISIBLE
+
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+        binding.tvBmiValue.text = bmiValue
+        binding.tvBmiType.text = bmiLabel
+        binding.tvBmiDesc.text = bmiDescription
+
     }
 }
